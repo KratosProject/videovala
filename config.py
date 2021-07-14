2712e20cb9eb7393837ec858c1f8269913417d08
NobiProject <87379521+NobiProject@users.noreply.github.com>
2021-07-14 07:27:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("1898326192:AAHWjNIRDAIPi1In2RxdvIocch8eDS7vGSU")
+APP_ID = int(os.environ.get("6191394"))
+API_HASH = os.environ.get("1b51d00a20c6ba6cdae9c04309d01a2e")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
